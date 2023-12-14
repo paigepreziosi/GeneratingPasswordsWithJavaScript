831cf46c8b4499ba4d0b7404c92453c883278f86
Paige Preziosi <paigepreziosi@Paiges-Laptop.lan>
2023-12-14 14:35:54 -0500

    commit script fix

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,15 @@ var symbols = confirm("Click OK to include special symbols in your password!");
 var numbers = confirm("Click OK to include numbers in your password!");
 var passwordLength = prompt("Choose a password length between from 8 to 128 characters!");
 
+
+
 // password variables
 var numberValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "V", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "<", ">", "?", ];
+
+
 
 // Write password to the #password input
 function writePassword() {
